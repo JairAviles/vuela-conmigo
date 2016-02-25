a8284ec08c084116a60d916e7dd164ec84d2427f
Jair Avilés <hi@jairaviles.mx>
2016-02-25 14:38:31 -0300

    [Add] Gulp tasks for sass compiling
    [Removed] css/* in ./scss folder

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,20 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-	console.log('Hello World');
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+
+// Styles Task
+// Sass
+gulp.task('sass', function () {
+ return gulp.src('./scss/style.scss')
+   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+   .pipe(gulp.dest('./css'));
 });
+
+// Watch Task
+// Watches JS
+gulp.task('watch', function(){
+	gulp.watch('./scss/style.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
