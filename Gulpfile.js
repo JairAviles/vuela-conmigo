f61a5149dc16af478e06c5ed591325d6130f0c3d
Jair Avilés <hi@jairaviles.mx>
2016-02-25 21:16:15 -0300

    [Fixed] Rename some scss files
    [Add] Gulp-plumber dependency on project for agile development

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,24 @@
 'use strict';
 
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	plumber = require('gulp-plumber');
 
 // Styles Task
 // Sass
 gulp.task('sass', function () {
- return gulp.src('./scss/style.scss')
-   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   .pipe(gulp.dest('./css'));
-});
+	return gulp.src('scss/**/*.scss')
+	.pipe(plumber())
+	.pipe(sass({
+		outputStyle: 'compressed'
+	}).on('error', sass.logError))
+	.pipe(gulp.dest('css'));
+	});
 
 // Watch Task
 // Watches JS
 gulp.task('watch', function(){
-	gulp.watch('./scss/style.scss', ['sass']);
-});
+	gulp.watch('scss/**/*.scss', ['sass']);
+	});
 
 gulp.task('default', ['sass', 'watch']);
