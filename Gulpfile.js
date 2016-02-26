cd8cb70bdfac003517b930b96b5892724121b898
Jair Avilés <hi@jairaviles.mx>
2016-02-26 09:29:10 -0300

    [Update] .gitignore for 'build' folder
    [Update] gulp-imagemin package and task added

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,23 +2,40 @@
 
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	plumber = require('gulp-plumber');
+	plumber = require('gulp-plumber'),
+	livereload = require('gulp-livereload'),
+	imagemin = require('gulp-imagemin');
+
+function errorLog(error) {
+	console.error.bind(error);
+	this.emit('end');
+}
 
 // Styles Task
 // Sass
 gulp.task('sass', function () {
 	return gulp.src('scss/**/*.scss')
-	.pipe(plumber())
 	.pipe(sass({
 		outputStyle: 'compressed'
-	}).on('error', sass.logError))
-	.pipe(gulp.dest('css'));
-	});
+	}).on('error', errorLog))//sass.logError))
+	.pipe(gulp.dest('css'))
+	.pipe(livereload());
+});
+
+// Image Task
+// Compress Images
+gulp.task('image', function(){
+	gulp.src('img/**')
+	.pipe(imagemin())
+	.pipe(gulp.dest('build/img'));
+});
 
 // Watch Task
 // Watches JS
 gulp.task('watch', function(){
+	livereload.listen();
 	gulp.watch('scss/**/*.scss', ['sass']);
-	});
+	gulp.watch('img/**', ['image']);
+});
 
 gulp.task('default', ['sass', 'watch']);
